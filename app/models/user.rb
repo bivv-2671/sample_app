35df74844ca476c3a02fecdc228e338c1a176cb5
vovanbi <vanbi010599@gmail.com>
2021-08-27 16:41:47 +0700

    basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,17 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :name, presence: true,
     length: {maximum: Settings.validation.name_max}
-  validates(:email, presence: true,
+  validates :email, presence: true,
     length: {maximum: Settings.validation.email_max},
     format: {with: VALID_EMAIL_REGEX},
-    uniqueness: true)
+    uniqueness: true
+
+  def User.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+    BCrypt::Engine::MIN_COST
+  else
+    BCrypt::Engine.cost
+  end
+    BCrypt::Password.create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
 
     get "sign-up", to:"users#new"
     post "sign-up", to:"users#create"
+    get "show", to:"users#show"
 
-    resources :users, only: %i(new create)
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+
+    resources :users, only: %i(new create show)
   end
 end
